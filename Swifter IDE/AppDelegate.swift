4d11869ef9c509efacd1a29b240a6893b80fd2ec
Your Name Here <you@yourdomain.com>
2019-03-24 17:20:38 -0600

    Initial Commit

---
+++ Swifter IDE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  2 IDE Swift FIle Editor
+//
+//  Created by DebuggingVM on 11/19/18.
+//  Copyright © 2018 Isaac Raval. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
